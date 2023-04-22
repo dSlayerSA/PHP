97256e6fb558c0f1161632779912f0a68e51d11b
Felipe <luizfelipesouza123@gmail.com>
2023-04-21 21:49:02 -0300

    FrontEnd template

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,71 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+    <v-app-bar app color="indigo darken-2" dark>
+      <v-btn icon @click="drawer = !drawer">
+        <v-icon>mdi-menu</v-icon>
       </v-btn>
+      <v-toolbar-title>My App</v-toolbar-title>
     </v-app-bar>
-
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-item @click="selectPage('alunos')">
+          <v-list-item-title>Alunos</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <v-card v-if="selectedPage === 'alunos'">
+          <v-card-title>Pesquisar Alunos
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="openAddAlunoDialog">Adicionar Aluno</v-btn>
+          </v-card-title>
+
+          <v-card-text>
+            <v-text-field v-model="searchText" label="Pesquise por RA ou CPF do aluno" outlined></v-text-field>
+            <v-btn @click="show" color="primary">Pesquisar</v-btn>
+          </v-card-text>
+
+          <v-data-table v-if="alunos.length > 0" :headers="headers" :items="alunos">
+            <template v-slot:RA="{ item }">
+              <v-text-field v-model="item.RA" dense outlined hide-details></v-text-field>
+            </template>
+            <template v-slot:nome="{ item }">
+              <v-text-field v-model="item.nome" dense outlined hide-details></v-text-field>
+            </template>
+            <template v-slot:cpf="{ item }">
+              <v-text-field v-model="item.cpf" dense outlined hide-details></v-text-field>
+            </template>
+            <template v-slot:email="{ item }">
+              <v-text-field v-model="item.email" dense outlined hide-details></v-text-field>
+            </template>
+          </v-data-table>
+
+          <v-alert v-else :value="true" color="warning" icon="mdi-alert-circle-outline">
+            Nenhum resultado encontrado.
+          </v-alert>
+
+          <v-dialog v-model="addAlunoDialog" max-width="500px">
+            <v-card>
+              <v-card-title>
+                Adicionar Aluno
+              </v-card-title>
+              <v-card-text>
+                <v-form ref="form">
+                  <v-text-field v-model="RA" label="RA" outlined></v-text-field>
+                  <v-text-field v-model="nome" label="Nome" outlined></v-text-field>
+                  <v-text-field v-model="cpf" label="CPF" outlined></v-text-field>
+                  <v-text-field v-model="email" label="Email" outlined></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="primary" @click="addAluno">Salvar</v-btn>
+                <v-btn @click="addAlunoDialog = false">Cancelar</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
